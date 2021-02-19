51aa23e9c962baed0b171ea8f12a88e905b57191
Erick <erick._cruz@outlook.com>
2021-02-18 21:47:50 -0500

    Modularizacion de componentes e intalacion de angular material material.angular.io

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -9,7 +9,6 @@ export class AdminGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return false;
+    return true;
   }
-  
 }
